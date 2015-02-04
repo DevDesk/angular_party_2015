204f2c6863cd7797466d2585ec212012fc96c584
Lenny Urbanowski <lenny@itslennysfault.com>
2015-02-04 08:09:52 -0800

    init

---
+++ js/app.js
@@ -0,0 +1,33 @@
+var myApp = angular.module('TacoApp',[]);
+
+myApp.run(function(){
+    console.log('App started.')
+});
+
+myApp.controller('TacoBuilder',['$scope',function($scope){
+    $scope.name="Steak Taco";
+    $scope.toppings=['steak','cheese','salsa','beans'];
+    console.log('controller loaded.');
+
+    $scope.addTopping = function(){
+        $scope.toppings.push($scope.newTopping);
+        $scope.newTopping="";
+    }
+
+    $scope.removeTopping = function(idx){
+        $scope.toppings.splice(idx,1);
+    }
+}]);
+
+myApp.controller('OtherThing',['$scope',function($scope){
+    console.log('loaded other thing');
+    $scope.myClass="btn-warning";
+    $scope.small=true;
+    $scope.primary=true;
+    $scope.happy=false;
+
+    $scope.changeClass = function(){
+        $scope.small = !$scope.small;
+        $scope.primary = !$scope.primary;
+    }
+}]);
